931271012147d63386d1b3f29bf4f8ab149b1731
Insane <ilya2002i52@mail.ru>
2021-09-27 21:54:42 +0300

    jsTimer

---
+++ script.js
@@ -0,0 +1,188 @@
+'use strict'
+
+
+const product = {
+    plainBurger: {
+        name: 'Гамбургер простой',
+        price: 10000,
+        kcall: 500,
+        amount: 0,
+        // getSum: function name(params) {
+            
+        // }
+        get Sum (){
+            return this.price * this.amount;
+        },
+        get Kcall(){
+            return this.kcall * this.amount;
+        }
+    },
+    freshBurger: {
+        name: 'Гамбургер FRESH',
+        price: 20500,
+        kcall: 600,
+        amount: 0,
+        get Sum (){
+            return this.price * this.amount;
+        },
+        get Kcall(){
+            return this.kcall * this.amount;
+        }
+    },
+    freshCombo: {
+        name: 'FRESH COMBO',
+        price: 31900,
+        kcall: 800,
+        amount: 0,
+        get Sum (){
+            return this.price * this.amount;
+        },
+        get Kcall(){
+            return this.kcall * this.amount;
+        }
+    }
+};
+
+const extraProduct = {
+    doubleMayonnaise:{
+        name:'Двойной майонез',
+        price: 1000,
+        kcall: 400
+    },
+    lettuce: {
+        name: 'Салатный лист',
+        price: 500,
+        kcall: 200
+    },
+    cheese: {
+        name:'Сыр',
+        price: 700,
+        kcall: 150
+    }
+}
+
+
+const btnPlusOrMinus = document.querySelectorAll('.main__product-btn');
+
+for(let element of btnPlusOrMinus){
+    element.addEventListener('click', function () {
+        plusOrMinus(this);
+    })
+}
+
+function plusOrMinus(element) {
+    
+    // closest() - метод подключается кродительскому элементу.
+    
+    const parent = element.closest('.main__product'),
+        parentId = parent.getAttribute('id'),
+        productAmount = parent.querySelector('.main__product-num'),
+        price = parent.querySelector('.main__product-price span'),
+        kcall = parent.querySelector('.main__product-kcall span'),
+        elementData = element.getAttribute('data-symbol');
+        
+        
+        if(elementData == '+'){
+            product[parentId].amount++;
+            
+            
+        }else if (elementData == '-' && product[parentId].amount > 0){
+            product[parentId].amount--;
+            
+        }
+        
+        productAmount.innerHTML = product[parentId].amount;
+        price.innerHTML = product[parentId].Sum;
+        kcall.innerHTML = product[parentId].Kcall;
+}
+
+const checkExtraProduct = document.querySelectorAll('.main__product-checkbox');
+
+for(let i = 0; i < checkExtraProduct.length ; i++){
+    checkExtraProduct[i].addEventListener('click', function(){
+        addExtraProduct(this);
+    })
+}
+
+
+function addExtraProduct(element){
+    const parent = element.closest('.main__product'),
+          parentId = parent.getAttribute('id'),
+          kcall = parent.quer-ySelector('.main__product-kcall span'),
+          price = parent.querySelector('.main__product-price span'),
+          elAttribute = element.getAttribute('data-extra');
+          
+          product[parentId][elAttribute] = element.checked;
+          
+          if(product[parentId][elAttribute] == true){
+              
+              product[parentId].price += extraProduct[elAttribute].price;
+              product[parentId].kcall += extraProduct[elAttribute].kcall;
+              
+          } else if(product[parentId][elAttribute] == false){
+            product[parentId].price -= extraProduct[elAttribute].price;
+            product[parentId].kcall -= extraProduct[elAttribute].kcall;
+          }
+          
+          price.innerHTML = product[parentId].Sum;
+          kcall.innerHTML = product[parentId].Kcall;
+}
+
+const addCart = document.querySelector('.addCart'),
+        receipt = document.querySelector('.receipt'),
+        receiptOut = document.querySelector('.receipt__window-out');
+        
+let arrProduct = [],
+    totalPrice = 0,
+    totalKcall = 0,
+    totalName = '';
+
+addCart.addEventListener('click', function(){
+    receipt.classList.remove('active');
+    receipt.style.display = 'flex';
+    receipt.style.opacity = 1;
+    
+    for(const key in product){
+        if(product[key].amount> 0){
+            arrProduct.push(product[key])
+            for(const newKey in product[key]){
+                if(product[key][newKey] === true){
+                    product[key].name += '\n' + extraProduct[newKey].name;
+                }
+            }
+        }
+        product[key].price = product[key].Sum;
+        product[key].kcall = product[key].Kcall;
+        
+    }
+    
+    for(let value of arrProduct){
+        let el = value;
+        totalPrice += el.price;
+        totalKcall += el.kcall;
+        totalName += '\n' + el.name + '\n';
+        
+    }
+    
+   receiptOut.innerHTML= `Вы купили: \n ${totalName} \n Каллорийность ${totalKcall} \n Стоимость ${totalPrice}` ;
+    
+})
+
+   
+
+const lvlTimer = document.querySelector('.header__timer-extra');
+
+
+function startLvlTimer(){
+    if(lvlTimer.innerHTML < 50){
+        lvlTimer.innerHTML++;
+        console.log(lvlTimer.innerHTML);
+        setTimeout( startLvlTimer, 300);
+    } else if(lvlTimer.innerHTML >= 50 && lvlTimer.innerHTML < 100 ){
+        lvlTimer.innerHTML++;
+        setTimeout( startLvlTimer, 1000);
+    }
+    
+}
+
+startLvlTimer();
